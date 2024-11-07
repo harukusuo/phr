eb87ab097036ffdbef8ed86c3602d0dd3e62eb56
sarah <sarahgabriellybatista@gmail.com>
2024-11-07 18:47:40 -0300

    Atualização 07/11 às 18:47

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// monkey patch window.fetch to change base URL
+const originalFetch = window.fetch;
+window.fetch = (url, options) => {
+  return originalFetch('https://pethelpapi.vercel.app' + url, options);
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
